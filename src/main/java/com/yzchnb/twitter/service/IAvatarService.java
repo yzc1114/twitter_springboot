cc89a3d3f1f128ffc98ff7567141bd2ef88a2418
Zhou <playerzhou@icloud.com>
2019-11-08 15:34:42 +0800

    update

--- src/main/java/com/yzchnb/twitter/service/IAvatarService.java
+++ src/main/java/com/yzchnb/twitter/service/IAvatarService.java
@@ -2,4 +2,6 @@ package com.yzchnb.twitter.service;
 
 public interface IAvatarService {
     void SetMainAvatar(int user_id,int avatar_id);
+    Integer GetMainAvatar(int user_id);
+    Integer AddAvatar(int user_id);
 }
--- src/main/java/com/yzchnb/twitter/service/impls/AvatarServiceImpl.java
+++ src/main/java/com/yzchnb/twitter/service/impls/AvatarServiceImpl.java
@@ -1,5 +1,7 @@
 package com.yzchnb.twitter.service.impls;
 
+import com.yzchnb.twitter.dao.FunctionCaller.AddAvatarCaller;
+import com.yzchnb.twitter.dao.FunctionCaller.FuncGetUserAvatarCaller;
 import com.yzchnb.twitter.dao.FunctionCaller.FuncSetMainAvatarCaller;
 import com.yzchnb.twitter.dao.FunctionMapper.FuncSetMainAvatarMapper;
 import com.yzchnb.twitter.service.IAvatarService;
@@ -10,8 +12,22 @@ import org.springframework.stereotype.Service;
 public class AvatarServiceImpl implements IAvatarService {
     @Autowired
     private FuncSetMainAvatarCaller funcSetMainAvatarCaller;
+    @Autowired
+    private FuncGetUserAvatarCaller funcGetUserAvatarCaller;
+    @Autowired
+    private AddAvatarCaller addAvatarCaller;
     @Override
     public void SetMainAvatar(int user_id, int avatar_id) {
         funcSetMainAvatarCaller.call(user_id,avatar_id);
+    }
+
+    @Override
+    public Integer AddAvatar(int user_id) {
+        return addAvatarCaller.call(user_id);
+    }
+
+    @Override
+    public Integer GetMainAvatar(int user_id) {
+        return funcGetUserAvatarCaller.call(user_id);
     }
 }
